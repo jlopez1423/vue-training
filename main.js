4817b50aa1b41a9e3378af7a11541e56163c0423
jlopez1423 <jlopez46@ucsc.edu>
2017-12-18 20:47:42 -0800

    Tutorial #8

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+Vue.component('task-list',{
+
+	template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+
+	data() {
+
+		return {
+
+			tasks: [
+				{ task: 'Go to the store', completed: true },
+
+                { task: 'Finish screencast', completed: false },
+
+                { task: 'Make donation', completed: false },
+
+			]
+		};
+	}
+
+
+});
+
+
 Vue.component('task',{
 
 	template: '<li><slot></slot></li>'
